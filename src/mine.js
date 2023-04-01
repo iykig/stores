a99e494121c355933cd83c39a23dc54725c5afc2
Niyaz <62894887+iykig@users.noreply.github.com>
2023-04-01 10:17:18 +0300

    up

---
+++ src/mine.js
@@ -0,0 +1,8 @@
+function menunav() {
+    var x = document.getElementById("n-menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
